f9aeeb96cdab26a461b3d5745ecf5aa43461801d
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2024-08-10 16:43:01 +0530

    add: parameter

--- handlers/appHandler.go
+++ handlers/appHandler.go
@@ -8,14 +8,13 @@ import (
 func AppHandler(w http.ResponseWriter, r *http.Request) {
     // Parse the query parameters
     param1 := r.URL.Query().Get("param_1")
-    param2 := r.URL.Query().Get("param_2")
 
-    if param1 == "" || param2 == "" {
-        http.Error(w, "Missing parameters", http.StatusBadRequest)
+    if param1 == "" {
+        http.Error(w, "Missing parameter", http.StatusBadRequest)
         return
     }
 
-    
-    response := fmt.Sprintf("Received parameters: param_1 = %s, param_2 = %s", param1, param2)
+
+    response := fmt.Sprintf("Received parameters: param_1 = %s" , param1)
     fmt.Fprintf(w, response)
 }
